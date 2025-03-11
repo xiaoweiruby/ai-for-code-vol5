ecb26e733de8705a6dae3fe8dc487db55486d3e3
xiaowei <xiaowei@192.168.2.85>
2025-03-11 23:17:20 +0800

    add rount three

--- subtracker/vite.config.js
+++ subtracker/vite.config.js
@@ -3,6 +3,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   plugins: [vue()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true
+      }
+    }
+  }
 })
